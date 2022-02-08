2e197f87f68705438db5292a10e4bdca1fedc0ad
Duayne Gordon <gordon@Duaynes-MacBook-Pro.local>
2022-02-08 16:43:52 -0500

    Color_Deconstruc

--- main.js
+++ main.js
@@ -31,32 +31,55 @@ if (UserColor_Program === 2) {
         console.log("Please enter a correct choice: 1, 2, or 3.");
         color_choice2 = Number(prompt("Please choose color one. Red = 1, blue = 2, yellow = 3 "));
     }
-    if ( color_choice2 = color_choice1){
-            console.log("please choose a different color");
+    if ( color_choice2 === color_choice1){
+            console.log("please choose a different color for choice 2, Thank You! ");
             color_choice2 = Number(prompt("Please choose color two. Red = 1, blue = 2, yellow = 3 ")); 
     }
-let mixed_color_Number = Number(color_choice1  + color_choice2); 
-// console.log(mixed_color_Number)
 
-if (mixed_color_Number === 3){
-     console.log("Your mixed color is Purple")
-}
-if (mixed_color_Number === 4){
-        console.log("Your mixed color is Orange")
-}
-if (mixed_color_Number === 5) {
-        console.log("Your mixed color is Green")
-}
-}   
+    let mixed_color_Number = Number(color_choice1  + color_choice2); 
+    // console.log(mixed_color_Number)
 
+    if (mixed_color_Number === 3){
+        console.log("Your mixed color is Purple.")
+    }
 
-
-
-
+    else if (mixed_color_Number === 4){
+        console.log("Your mixed color is Orange.")
+        }
+    else if (mixed_color_Number === 5) {
+        console.log("Your mixed color is Green.")
+        }
+   
+};
 
 
 
 
 /* If the user chooses to deconstruct, prompt them for a single color. Deconstruct the color (using if, else if, and else statements) then log the result.
  */
+
+if (UserColor_Program === 1){
+    let color_deconstruction = prompt("please choose a color to deconstruct. Choose 1 for Purple, choose 2 for Orange, or 3 for Green ");
+
+    if (color_deconstruction < 0 && color_deconstruction > 3){
+        console.log("Your choice is not within the range. Please choose again")
+        color_deconstruction = prompt("please choose a color to deconstruct. Choose 1 for Purple, choose 2 for Orange, or 3 for Green. ");
+        console.log("+++++ " + color_deconstruction)
+    }
+    if (color_deconstruction == 1){
+        console.log("The deconstructed colors are: Red and Blue.")
+    }
+
+    if (color_deconstruction == 2){
+        console.log("The deconstructed colors are: Red and Yellow.")
+        }
+    
+    if (color_deconstruction == 3){
+        console.log("The deconstructed colors are: Blue and Yellow")
+    } 
+    
+};
+
+
+
 
